54f673872360d6abae98efd117dc4af760618fbd
Alexis Luque <aluque@indexlabs.com.uy>
2017-10-06 18:19:35 -0300

    Remove auth0.js from basic-webapp

--- 00-Starter-Seed/basic-webapp/logout.php
+++ 00-Starter-Seed/basic-webapp/logout.php
@@ -7,13 +7,19 @@ $domain        = getenv('AUTH0_DOMAIN');
 $client_id     = getenv('AUTH0_CLIENT_ID');
 $client_secret = getenv('AUTH0_CLIENT_SECRET');
 $redirect_uri  = getenv('AUTH0_CALLBACK_URL');
+$audience      = getenv('AUTH0_AUDIENCE');
+
+if($audience == ''){
+    $audience = 'https://' . $domain . '/userinfo';
+}
 
 $auth0 = new Auth0([
   'domain' => $domain,
   'client_id' => $client_id,
   'client_secret' => $client_secret,
   'redirect_uri' => $redirect_uri,
-  'audience' => 'https://' . $domain . '/userinfo',
+  'audience' => $audience,
+  'scope' => 'openid profile',
   'persist_id_token' => true,
   'persist_refresh_token' => true,
 ]);
