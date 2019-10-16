688e037ba66d96da09de9e454a1649c1bc8235be
Josh Cunningham <josh@joshcanhelp.com>
2019-10-16 16:06:43 -0700

    Fix dotenv loader for new major version

--- 00-Starter-Seed/dotenv-loader.php
+++ 00-Starter-Seed/dotenv-loader.php
@@ -1,7 +1,7 @@
 <?php
   // Read .env
   try {
-    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv = Dotenv\Dotenv::create(__DIR__);
     $dotenv->load();
   } catch(InvalidArgumentException $ex) {
     // Ignore if no dotenv
